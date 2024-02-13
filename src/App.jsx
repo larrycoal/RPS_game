2dbf99c8436d93a0fab59c178580859143ee360c
Olayiwola Olanrewaju <larry_coal@outlook.com>
2024-02-13 10:35:50 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to the ultimate game of chance</h1>
+        <div>
+          <p>Rock, Paper, Scissors</p>
+          <button>Let's Go</button>
+        </div>
+        <div>
+          <p>Rock, Paper, Scissors,Lizard, Spock</p>
+          <button>Let's Go</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
